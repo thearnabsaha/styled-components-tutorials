c5c70bae96452903bd238dc6d62a5cb5ccfda647
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-28 09:42:21 +0530

    s1

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 const Heading = styled.h1`
   color: ${(props)=>(props.variant==="new"?"green":"red")};
 `;
@@ -8,14 +9,29 @@ const Heading2 = styled(Heading)`
   color: ${(props)=>(props.variant==="new"?"orange":"blue")};
   }
 `;
+const Heading3 = styled.h1`
+  color: ${(props)=>props.theme.dark.primary};
+`;
+// Define what props.theme will look like
+const theme = {
+  dark: {
+    primary:"purple",
+  },
+  light: {
+    primary:"pink",
+  },
+};
 const index = () => {
   return (
     <>
-      <h1>Arnab</h1>
-      <Heading >Arnab saha</Heading>
-      <Heading as="a">Arnab saha</Heading>
-      <Heading variant="new">Arnab saha</Heading>
-      <Heading2>Arnab saha</Heading2>
+      <ThemeProvider theme={theme}>
+        <h1>Arnab</h1>
+        <Heading >Arnab saha</Heading>
+        <Heading as="a">Arnab saha</Heading>
+        <Heading variant="new">Arnab saha</Heading>
+        <Heading2>Arnab saha</Heading2>
+        <Heading3>Arnab saha</Heading3>
+      </ThemeProvider>
     </>
   );
 }
